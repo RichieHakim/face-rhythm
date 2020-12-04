221496a155660584bd1e6ba02148fbdbfc0d88d8
akshay-jaggi <akshay.x.jaggi@gmail.com>
2020-12-04 16:45:18 -0500

    Package Management
    Let's manage some packages

--- setup.py
+++ setup.py
@@ -1,10 +1,16 @@
 from setuptools import find_packages, setup
 
 setup(
-    name='src',
+    name='face_rhythm',
     packages=find_packages(),
     version='0.1.0',
-    description='Project structure for Rich Hakim's Face Rhythms',
+    description="Project structure for Face Rhythms",
     author='Rich Hakim',
     license='MIT',
+    install_requires=['torch','torchvision','torchaudio',
+                      'jupyterlab', 'tensorly', 'opencv-python',
+                      'imageio', 'matplotlib','scikit-learn',
+                      'scikit-image'
+
+    ]
 )
