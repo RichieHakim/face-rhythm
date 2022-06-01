0a80ba7915547e923fd8f573294fb740e9fe6cd1
RichieHakim <RichHakim@gmail.com>
2022-06-01 13:56:55 -0400

    batch_run

--- batch_run/FRbatch.py
+++ batch_run/FRbatch.py
@@ -1,12 +1,61 @@
+"""
+Expects inputs deriving from a partial run of the main face-rhythm notebook. 
+
+Expects the following inputs:
+1. path to a .nwb file containing the initial dot coordinates
+2. path to videos
+3. path to a reference config file containing all the desired settings
+4. path to output directory
+
+- The reference config file can/should come from a local run of face-rhythm
+- The .nwb file should come from a partial run of face-rhythm
+
+
+This script will:
+1. make a new config fig with the paths taken from the old config file for the following: .nwb, videos, config file, and outputs
+"""
+
+
+
 from pathlib import Path
 
 
-path_configTemplate = '/media/rich/bigSSD/analysis_data/face_rhythm_paper/fig_4/2pRAM_motor_mapping/AEG21/2022_05_13/face_rhythm_20220513_movie3/configs/config_run.yaml'
-path_oldNWB         = '/media/rich/bigSSD/analysis_data/face_rhythm_paper/fig_4/2pRAM_motor_mapping/AEG21/2022_05_13/face_rhythm_20220513_movie3/data/sessionrun.nwb'
+# path_configTemplate = '/media/rich/bigSSD/analysis_data/face_rhythm_paper/fig_4/2pRAM_motor_mapping/AEG21/2022_05_13/face_rhythm_20220513_movie3/configs/config_run.yaml'
+# path_oldNWB         = '/media/rich/bigSSD/analysis_data/face_rhythm_paper/fig_4/2pRAM_motor_mapping/AEG21/2022_05_13/face_rhythm_20220513_movie3/data/sessionrun.nwb'
 
-dir_FRproject       = '/media/rich/bigSSD/analysis_data/face_rhythm_paper/fig_4/2pRAM_motor_mapping/AEG21/2022_05_13/batchRun'
-dir_videos          = '/media/rich/bigSSD/analysis_data/face_rhythm_paper/fig_4/2pRAM_motor_mapping/AEG21/2022_05_13/camera/'
-fileName_strMatch   = 'movie3'
+# dir_FRproject       = '/media/rich/bigSSD/analysis_data/face_rhythm_paper/fig_4/2pRAM_motor_mapping/AEG21/2022_05_13/batchRun'
+# dir_videos          = '/media/rich/bigSSD/analysis_data/face_rhythm_paper/fig_4/2pRAM_motor_mapping/AEG21/2022_05_13/camera/'
+# fileName_strMatch   = 'movie3'
+
+import sys
+path_script, path_params, dir_save = sys.argv
+
+import json
+with open(path_params, 'r') as f:
+    params = json.load(f)
+
+import shutil
+shutil.copy2(path_script, str(Path(dir_save) / Path(path_script).name));
+
+# params_template = {
+#     'dir_face_rhythm': dir_github,
+#     'dir_videos': dir_videos,  ## directory containing the video(s) matching the fileName_strMatch
+#     'fileName_strMatch' : 'movie3',
+#     'name_FRproject': 'batchRun',
+
+#     'path_configTemplate' : '/media/rich/bigSSD/analysis_data/face_rhythm_paper/fig_4/2pRAM_motor_mapping/AEG21/2022_05_13/face_rhythm_20220513_movie3/configs/config_run.yaml',
+#     'path_oldNWB'         : '/media/rich/bigSSD/analysis_data/face_rhythm_paper/fig_4/2pRAM_motor_mapping/AEG21/2022_05_13/face_rhythm_20220513_movie3/data/sessionrun.nwb',
+# }
+
+import sys
+dir_face_rhythm = params['dir_face_rhythm']
+sys.path.append(dir_face_rhythm)
+
+# from face_rhythm import ca2p_preprocessing, path_helpers, similarity, pickle_helpers, misc, featurization
+
+
+path_self, dir_FRproject, dir_videos, fileName_strMatch, path_oldNWB, path_configTemplate = sys.argv
+path_dispatcher_remote, dir_saveOutputs, path_script_remote, name_job, name_slurm, dir_videos = sys.argv
 
 nameRun = '_batch'
 
