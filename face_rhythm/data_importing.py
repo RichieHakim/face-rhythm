eb5cc3ee13ee9ac6ab202607872d9995e7fd43d5
RichieHakim <RichHakim@gmail.com>
2022-11-20 01:58:29 -0500

    added VideoReaderWrapper

--- face_rhythm/data_importing.py
+++ face_rhythm/data_importing.py
@@ -4,6 +4,7 @@ from typing import List
 
 import numpy as np
 from tqdm import tqdm
+import decord
 
 from .util import FR_Module
 
@@ -18,7 +19,6 @@ class Dataset_videos(FR_Module):
     ):
         ## Imports
         super().__init__()
-        import decord
 
         ## Set variables
         self.contiguous = bool(contiguous)
@@ -36,13 +36,13 @@ class Dataset_videos(FR_Module):
         self.paths_videos = paths_videos if isinstance(paths_videos, list) else [paths_videos]
 
         ## Assert that all paths_videos exist
-        exists_paths_videos = [Path(path).exists() for path in self.paths_videos]
+        exists_paths_videos = [bool(Path(path).exists()) for path in self.paths_videos]
         assert all(exists_paths_videos), f"FR ERROR: paths_videos must exist. The following paths do not exist: {[path for path, exists in zip(self.paths_videos, exists_paths_videos) if not exists]}"
 
 
         ## Load videos
         print("FR: Loading lazy video reader objects...") if self.verbose > 1 else None
-        self.videos = [decord.VideoReader(path_video, ctx=decord.cpu(0)) for path_video in tqdm(self.paths_videos, disable=(self.verbose < 2))]
+        self.videos = [VideoReaderWrapper(path_video, ctx=decord.cpu(0)) for path_video in tqdm(self.paths_videos, disable=(self.verbose < 2))]
 
         ## make video metadata dataframe
         self.metadata = {
@@ -57,17 +57,22 @@ class Dataset_videos(FR_Module):
         if frame_rate_clamp is None:
             frame_rates = self.metadata["frame_rate"]
             ## warn if any video's frame rate is very different from others
-            max_diff = (np.max(frame_rates) - np.min(frame_rates)) / np.mean(frame_rates)
+            max_diff = float((np.max(frame_rates) - np.min(frame_rates)) / np.mean(frame_rates))
             print(f"FR WARNING: max frame rate difference is large: {max_diff*100:.2f}%") if ((max_diff > 0.1) and (self.verbose > 0)) else None
             self.frame_rate = float(np.median(frame_rates))
         else:
             self.frame_rate = float(frame_rate_clamp)
 
         ## For FR_Module compatibility
-        self.run_info = {
-            "paths_videos": self.paths_videos,
+        self.config = {
+            "paths_videos": paths_videos,
             "contiguous": contiguous,
             "frame_rate_clamp": frame_rate_clamp,
+            "verbose": verbose,
+        }
+        self.run_info = {
+            "exists_paths_videos": exists_paths_videos,
+            "max_diff": max_diff,
         }
         self.run_data = {
             "frame_rate": self.frame_rate,
@@ -82,3 +87,19 @@ class Dataset_videos(FR_Module):
     def __len__(self): return len(self.videos)
     def __iter__(self): return iter(self.videos)
     def __next__(self): return next(self.videos)
+
+
+class VideoReaderWrapper(decord.VideoReader):
+    """
+    Used to fix a memory leak bug in decord.VideoReader
+    Taken from here.
+    https://github.com/dmlc/decord/issues/208#issuecomment-1157632702
+    """
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.seek(0)
+
+    def __getitem__(self, key):
+        frames = super().__getitem__(key)
+        self.seek(0)
+        return frames
--- face_rhythm/util.py
+++ face_rhythm/util.py
@@ -4,6 +4,10 @@ class FR_Module:
         self.run_info = None
         self.run_data = None
 
+        ## Get module name
+        self.module_name = self.__class__.__name__
+
+
     def save_run_info(
         self, 
         path_run_info=None, 
@@ -21,13 +25,15 @@ class FR_Module:
                 If the file does not exist, it will be created.
             overwrite (bool):
                 If True, overwrites fields within the run_info.yaml file.
+            verbose (int):
+                Verbosity level. 0 is silent. 1 is print warnings. 2 is print all.
         """
         import yaml
         from pathlib import Path
 
         ## Assert if self.run_info and self.run_data are not None
-        assert self.run_info is not None, 'FR: self.run_info is None. Module likely did not run properly. Please set self.run_info before saving.'
-        assert self.run_data is not None, 'FR: self.run_data is None. Module likely did not run properly. Please set self.run_data before saving.'
+        assert self.run_info is not None, 'FR ERROR: self.run_info is None. Module likely did not run properly. Please set self.run_info before saving.'
+        assert self.run_data is not None, 'FR ERROR: self.run_data is None. Module likely did not run properly. Please set self.run_data before saving.'
 
         ## Assert that either path_run_info or path_config must be a string, but not both
         assert (path_run_info is not None) and (path_config is None) or (path_run_info is None) and (path_config is not None), "FR ERROR: Either path_run_info or path_config must be specified as a string, but not both"
@@ -47,9 +53,6 @@ class FR_Module:
         ## Set path_run_info. Get from config if path_run_info is None
         path_run_info = load_yaml_safe(path_config)["paths"]["run_info"] if path_run_info is None else path_run_info
 
-        ## Get module name
-        module_name = self.__class__.__name__
-
         ## Check if file exists and load it if it does
         ## If directory to file does not exist, create it
         if Path(path_run_info).exists()==False:
@@ -61,14 +64,14 @@ class FR_Module:
             run_info = load_yaml_safe(path_run_info)
             
         ## Append self.run_info to module_name key in run_info.yaml
-        if (module_name in run_info.keys()) and not overwrite:
-            print(f'FR Warning: {module_name} already in run_info.yaml. Not overwriting.') if verbose > 0 else None
-        elif (module_name in run_info.keys()) and overwrite:
-            print(f'FR Warning: {module_name} already in run_info.yaml. Overwriting.') if verbose > 0 else None
-            run_info[module_name] = self.run_info
+        if (self.module_name in run_info.keys()) and not overwrite:
+            print(f'FR Warning: {self.module_name} already in run_info.yaml. Not overwriting.') if verbose > 0 else None
+        elif (self.module_name in run_info.keys()) and overwrite:
+            print(f'FR Warning: {self.module_name} already in run_info.yaml. Overwriting.') if verbose > 0 else None
+            run_info[self.module_name] = self.run_info
         else:
-            print(f'FR: Adding {module_name} to run_info.yaml') if verbose > 1 else None
-            run_info[module_name] = self.run_info
+            print(f'FR: Adding {self.module_name} to run_info.yaml') if verbose > 1 else None
+            run_info[self.module_name] = self.run_info
 
         ## Save run_info.yaml file
         print(f'FR: Saving run_info.yaml to {path_run_info}') if verbose > 1 else None
@@ -76,6 +79,57 @@ class FR_Module:
             yaml.dump(run_info, f, Dumper=yaml.Dumper, sort_keys=False)
 
 
+    def save_config(
+        self, 
+        path_config=None, 
+        overwrite=True, 
+        verbose=1
+    ):
+        """
+        Appends the self.config dictionary to the config.yaml file.
+        This dictionary is created by the subclass and should contain
+         all the parameters used to run the module.
+        RH 2022
+
+        Args:
+            path_config (str):
+                Path to config.yaml file.
+                If the file does not exist, it will be created.
+            overwrite (bool):
+                If True, overwrites fields within the config.yaml file.
+            verbose (int):
+                Verbosity level. 0 is silent. 1 is print warnings. 2 is print all.
+        """
+        import yaml
+        from pathlib import Path
+
+        ## Assert if self.config is not None
+        assert self.config is not None, 'FR ERROR: self.config is None. Module likely did not run properly. Please set self.config before saving.'
+
+        ## Assert that path_config is a string, exists, is a file, is a yaml file, and is named properly
+        assert isinstance(path_config, str), "FR ERROR: path_config must be a string"
+        assert Path(path_config).exists(), "FR ERROR: path_config must exist"
+        assert Path(path_config).is_file(), "FR ERROR: path_config must be a file"
+        assert Path(path_config).suffix == ".yaml", "FR ERROR: path_config must be a yaml file"
+        assert Path(path_config).name == "config.yaml", "FR ERROR: path_config must be named config.yaml"
+
+        config = load_yaml_safe(path_config)
+            
+        ## Append self.config to module_name key in config.yaml
+        if (self.module_name in config.keys()) and not overwrite:
+            print(f'FR Warning: {self.module_name} already in config.yaml. Not overwriting.') if verbose > 0 else None
+        elif (self.module_name in config.keys()) and overwrite:
+            print(f'FR Warning: {self.module_name} already in config.yaml. Overwriting.') if verbose > 0 else None
+            config[self.module_name] = self.config
+        else:
+            print(f'FR: Adding {self.module_name} to config.yaml') if verbose > 1 else None
+            config[self.module_name] = self.config
+
+        ## Save config.yaml file
+        print(f'FR: Saving config.yaml to {path_config}') if verbose > 1 else None
+        with open(path_config, 'w') as f:
+            yaml.dump(config, f, Dumper=yaml.Dumper, sort_keys=False)
+            
 
 
 def load_yaml_safe(path, verbose=0):
