76216f0bb0d2977c21b173a61a62b5574a3a68c6
RichieHakim <RichHakim@gmail.com>
2024-01-28 17:54:15 -0500

    fix data_test stuff

--- tests/test_environment.py
+++ tests/test_environment.py
@@ -4,7 +4,7 @@ import re
 
 def test_python_version(
     look_for_verion_in_environmentYaml=True, 
-    filename_environmentYaml='environment_GPU.yml',
+    filename_environmentYaml='environment.yml',
     fallback_version=3.11,
     verbose=1,
 ):
